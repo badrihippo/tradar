2f00ed1e2f50bb4b791b464b49feab5edfbcdfbe
Hippo <badrihippo@gmail.com>
2017-02-07 17:28:57 +0530

    Create basic profile page
    Doesn't display much info apart from the name, because the *is* no
    more info in the data model, at the moment

--- tradar/views.py
+++ tradar/views.py
@@ -1,11 +1,17 @@
 from flask import (
     url_for,
-    render_template
+    render_template,
+    abort,
 )
 from .app import app
 from .auth import (
     current_user,
     login_required
+)
+from .models import (
+    Country,
+    Account,
+    Person
 )
 
 @app.route('/')
@@ -13,3 +19,12 @@ def index():
     if current_user.is_authenticated:
         return render_template('home.htm')
     return render_template('index.htm')
+
+@app.route('/@<username>/')
+def profile(username):
+    try:
+        account = Account.nodes.get(username=username)
+    except Account.DoesNotExist:
+        abort(404)
+    person = account.owner.get()
+    return render_template('profile.htm', person=person)
