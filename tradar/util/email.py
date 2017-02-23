73b96dec00c84317e5c93b004167f1ade63dd6ef
Hippo <badrihippo@gmail.com>
2017-02-23 17:00:33 +0530

    Enable email debug logging, if sending is suppressed

--- tradar/util/email.py
+++ tradar/util/email.py
@@ -1,6 +1,7 @@
 from flask.ext.mail import (
     Mail,
     Message,
+    email_dispatched
 )
 from ..app import app
 mail = Mail(app)
@@ -27,3 +28,14 @@ def send_email(message, subject=None, sender=None, recipients=[]):
         msg = Message(subject, sender=sender, recipients=recipients)
         msg.body = message,
         mail.send(message)
+
+if app.config.get('MAIL_SUPPRESS_SEND'):
+    def log_message(msg, app):
+        out = '\n'.join(('Sending email',
+            'From: %s' % msg.sender,
+            'To: %s' % ', '.join([r for r in msg.recipients]),
+            'Subject: %s' % msg.subject,
+            msg.body))
+        app.logger.debug(out)
+
+    email_dispatched.connect(log_message)
