0d80434ddf488cf0e624a97eda96ef843b8bf474
Hippo <badrihippo@gmail.com>
2017-03-17 15:31:04 +0530

    Set up email login procedure
    Link is emailed to user, who then clicks on it for a one-time login.

--- tradar/auth.py
+++ tradar/auth.py
@@ -25,6 +25,10 @@ from .util.email import (
     Message,
 )
 
+from .util.security import ts
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -44,7 +48,8 @@ def login():
         user = load_user(form.username.data)
         if user is None:
             # Pretend to have sent the sign-in email
-            return render_template('accounts/login_emailsent.htm')
+            return render_template('accounts/login_emailsent.htm',
+                username=form.username.data)
         elif not user.is_active:
             return render_template('accounts/login_deactivated.htm')
         elif user.signin_mode == 'password':
@@ -53,25 +58,76 @@ def login():
                 form=password_form,
                 name=user.owner.get().full_name)
         elif user.signin_mode == 'email':
-            # TODO: Send signin email
-            return render_template('accounts/login_emailsent.htm')
+            # Generate signin link
+            data = {
+                'last_login': datetime.strftime(user.last_login, DATE_FORMAT),
+                'username': str(user.username),
+            }
+            token = ts.dumps(data, salt='login-key')
+            confirm_url = url_for('login_withemail',
+                token=token,
+                _external=True)
+            # Send the email
+            msg = Message()
+            msg.subject = 'Sign in to Tradar'
+            msg.recipients.append(user.email)
+            msg.body=render_template('emails/login_email.txt',
+                name=user.username,
+                confirm_url=confirm_url)
+            msg.html=render_template('emails/login_email.htm',
+                name=user.username,
+                confirm_url=confirm_url)
+            mail.send(msg)
+            return render_template('accounts/login_emailsent.htm',
+                name=user.username,
+                confirm_url=confirm_url)
         else:
             flash('There was an error signing in. Please try again.')
     return render_template('accounts/login.htm', form=form)
 
+@app.route('/l/email/<token>/')
+def login_withemail(token):
+    # Load token
+    try:
+        # 900 seconds = 15 minutes
+        data = ts.loads(token, salt='login-key', max_age=900)
+    except:
+        abort(404)
+
+    # Load user
+    user = load_user(data['username'])
+
+    # Check validity
+    if user is None or user.signin_mode != 'email':
+        abort(404)
+
+    # Confirm that last_login still matches
+    # If not, it means the user has already signed in,
+    # in which case the token should be considered invalid.
+    if datetime.strftime(user.last_login, DATE_FORMAT) != data['last_login']:
+        return render_template('accounts/login_expired.htm')
+
+    # Everything OK. Log in user.
+    user.last_login = datetime.now()
+    user.save()
+    login_user(user)
+    return redirect(request.args.get('next') or '/')
+
+
 @app.route('/l/pass/', methods=['POST'])
 def login_withpassword():
     form = PasswordForm()
+    user = load_user(form.username.data)
+    if (user is None) or not user.check_password(form.password.data):
+        msg = 'Invalid password'
+        if 'password' in form.errors:
+            form.errors['password'].append(msg)
+        else:
+            form.errors['password'] = [msg]
+
     if form.validate_on_submit():
         # Login and validate user
-        user = load_user(form.username.data)
-        if (user is None) or not user.check_password(form.password.data):
-            msg = 'Invalid password'
-            if 'password' in form.errors:
-                form.errors['password'].append(msg)
-            else:
-                form.errors['password'] = [msg]
-        elif not user.is_active:
+        if not user.is_active:
            return render_template('accounts/login_deactivated.htm')
         else:
             # All OK. Set last_login and login user
